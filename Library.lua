3305b6d6ae0a226bf5300000ee331e7ee6610f20
BlackDisciplesHook <ilovefarts3123123@gmail.com>
2025-11-18 02:30:16 +0100

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1156,7 +1156,7 @@ do
 
             ContainerLabel.Text = string.format('[%s] %s (%s)', KeyPicker.Value, Info.Text, KeyPicker.Mode);
 
-            ContainerLabel.Visible = true;
+            ContainerLabel.Visible = KeyPicker.NoUI;
             ContainerLabel.TextColor3 = State and Library.AccentColor or Library.FontColor;
 
             Library.RegistryMap[ContainerLabel].Properties.TextColor3 = State and 'AccentColor' or 'FontColor';
